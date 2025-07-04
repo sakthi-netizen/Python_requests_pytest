f2d6197406f4369a76ac59774ed80f7c88f3581d
srisakthi-m-15386 <srisakthi.m@zohocorp.com>
2025-07-04 22:10:50 +0530

    Python Requests Pytest Base Framework

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+import os
+import json
+from datetime import datetime
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    report_dir = "reports"
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    config.option.htmlpath = f"{report_dir}/report_{now}.html"
+
+@pytest.fixture
+def load_user_data():
+    json_file_path = os.path.join(os.path.dirname(__file__),"data","test_data.json")
+    with open(json_file_path) as json_file:
+        data = json.load(json_file)
+    return data
+
+@pytest.fixture(scope="session",autouse=True)
+def setup_teardown():
+    print("\nSetting up the test environment")
+    yield
+    print("\nTearing down the test environment")
